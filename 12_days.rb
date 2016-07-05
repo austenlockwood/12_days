66f0463de3693c79f0fb9141e0c5a177597a0613
Austen Lockwood <austenlockwood@gmail.com>
2016-07-05 11:31:18 -0400

    Refactored so there is a template of the opening statement and the unique statements are broken into variables.  If statements in the verse method now string together these variables.

--- 12_days.rb
+++ 12_days.rb
@@ -13,66 +13,63 @@ def entire_song
   verse(12)
 end
 
+def template(day)
+  "On the #{day} of Christmas, my true love gave to me "
+end
+
 def verse(number)
+
+  day_one = "a partridge in a pear tree."
+  day_two = "two turtle doves, and "
+  day_three = "three French hens, "
+  day_four = "four calling birds, "
+  day_five = "FIVE GOOOOLDEN RIIIIIINGS, "
+  day_six = "six geese a laying, "
+  day_seven = "seven swans a swimming, "
+  day_eight = "eight maids a milking, "
+  day_nine = "nine ladies dancing, "
+  day_ten = "ten lords a leaping, "
+  day_eleven = "eleven pipers piping, "
+  day_twelve = "twelve drummers drumming, "
+
   if number == 1
-    puts "On the first day of Christmas, my true love gave to me a partridge in a pear tree.
-    "
+    puts template(number) + day_one
   elsif number == 2
-    puts "On the second day of Christmas my true love gave to me two turtle doves
-    and a partridge in a pear tree.
-    "
+    puts template(number) + day_two + day_one
   elsif number == 3
-    puts "On the third day of Christmas my true love gave to me three French hens,
-    two turtle doves, and a Partridge in a pear tree.
-    "
+    puts template(number) + day_three + day_two + day_one
   elsif number == 4
-    puts "On the fourth day of Christmas my true love gave to me four calling birds,
-    three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_four + day_three + day_two + day_one
   elsif number == 5
-    puts "On the fifth day of Christmas my true love gave to me five golden rings,
-    four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_five + day_four + day_three + day_two + day_one
   elsif number == 6
-    puts "On the sixth day of Christmas my true love gave to me six geese a laying,
-    five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_six + day_five + day_four + day_three + day_two + day_one
   elsif number == 7
-    puts "On the seventh day of Christmas my true love gave to me seven swans a swimming,
-    six geese a laying, five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_seven + day_six + day_five + day_four + day_three + day_two + day_one
   elsif number == 8
-    puts "On the eighth day of Christmas my true love gave to me eight maids a milking,
-    seven swans a swimming, six geese a laying, five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_eight + day_seven + day_six + day_five + day_four + day_three + day_two + day_one
   elsif number == 9
-    puts "On the ninth day of Christmas my true love gave to me nine ladies dancing,
-    eight maids a milking, seven swans a swimming, six geese a laying, five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_nine + day_eight + day_seven + day_six + day_five + day_four + day_three + day_two + day_one
   elsif number == 10
-    puts "On the tenth day of Christmas my true love gave to me ten lords a leaping,
-    nine ladies dancing, eight maids a milking, seven swans a swimming, six geese a laying, five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_ten + day_nine + day_eight + day_seven + day_six + day_five + day_four + day_three + day_two + day_one
   elsif number == 11
-    puts "On the eleventh day of Christmas my true love gave to me eleven pipers piping,
-    ten lords a leaping nine ladies dancing, eight maids a milking, seven swans a swimming, six geese a laying, five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
+    puts template(number) + day_eleven + day_ten + day_nine + day_eight + day_seven + day_six + day_five + day_four + day_three + day_two + day_one
   elsif number == 12
-    puts "On the twelfth day of Christmas my true love gave to me twelve drummers drumming,
-    eleven pipers piping, ten lords a leaping nine ladies dancing, eight maids a milking, seven swans a swimming, six geese a laying, five golden rings, four calling birds, three French hens, two turtle doves and a Partridge in a pear tree.
-    "
-  elsif number == 0 || number == ""
-    entire_song
+    puts template(number) + day_twelve + day_eleven + day_ten + day_nine + day_eight + day_seven + day_six + day_five + day_four + day_three + day_two + day_one
   else
-    puts "There is no verse #{number}."
+    entire_song
   end
+
 end
 
 def input
-  puts "what verse are you in the song. Enter 0 to print the whole song"
+  puts "what verse are you in the song. Enter 100 to print the whole song."
   answer = gets.chomp
-  answer = answer.to_i
-  verse(answer)
+  if answer.to_i.to_s == answer
+    verse(answer.to_i)
+  else
+    puts "There is no verse #{answer}."
+  end
 end
 
 input
